c585b27bdd79719a56c26551cf369cdd4cb58761
Taki Emmanuel <takipemmanuel@gmail.com>
2025-08-08 13:47:00 +0100

    fix: bind server to 0.0.0.0 for render compatibility

--- server_side/index.js
+++ server_side/index.js
@@ -100,6 +100,7 @@ function broadcast(message, senderId) {
 }
 
 const port = 3000
-server.listen(port, () => {
+server.listen(port,'0.0.0.0', () => {
   console.log(`Websocket server listening on port ${port}`)
 })
+
